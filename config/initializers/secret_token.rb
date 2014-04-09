73023e28a586f4fa128b1da8766e05ae5d432194
Thomas THIMOTHEE <thomas@oxynum.fr>
2014-04-09 18:46:07 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ducky::Application.config.secret_key_base = '9e48268959ed589233469a62ab8eff22a043bee557d6ada030c97bee828abed2ad6ee9f4dd2cecbd8aa0b1033d380d1795294c8950e1dc176fc968af7c9e5bec'
