788f8254a9090bb8c735b98f280b262fea56f9a8
Thomas THIMOTHEE <thomas@oxynum.fr>
2014-04-09 19:19:51 +0900

    add some output formats

--- app/controllers/configuration_files_controller.rb
+++ app/controllers/configuration_files_controller.rb
@@ -2,19 +2,20 @@ class ConfigurationFilesController < ApplicationController
 
   def show
     @file = ConfigurationFile.find params[:id]
-    @xml_string = File.read('wix_files/#{@file.id}.wix')
+    @xml_string = File.read("wix_files/#{@file.id}.wix")
     @property_hash = Hash.from_xml(@xml_string)
     respond_to do |format|
       format.json {render json: @property_hash.to_json}
       format.xml {render xml: @xml_string}
-      # format.csv {
-        # @csv = CSV.generate do |csv|
-        #   all.each do ||
-        #     csv << @property_hash.values
-        #   end
-        # end
-      # }
-      # format.xls {}
+      format.csv {
+        @csv = CSV.generate do |csv|
+          csv << ["keyword", "target"]
+          @property_hash["WIX"]["body"]["entry"].each do |entry|
+            csv << [entry["keyword"], entry["target"]]
+          end
+        end
+        send_data @csv
+      }
     end
   end
 
@@ -23,7 +24,7 @@ class ConfigurationFilesController < ApplicationController
     respond_to do |format|
       format.json {render json: @files}
       format.xml {render xml: @files}
-      format.csv {render csv: @files.to_csv}
+      format.csv {send_data @files.to_csv}
       format.xls { send_data @files.to_csv(col_sep: "\t") }
     end
   end
